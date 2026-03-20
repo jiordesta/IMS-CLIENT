aa62bdc10500a9fa3dcf609c5d7482cbd54675bf
Irson <jiordesta@gmail.com>
2026-03-20 09:43:50 +0800

    update api base url

--- src/configs/axiosInstance.ts
+++ src/configs/axiosInstance.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 export const axiosInstance = axios.create({
-  baseURL: "http://localhost:3000/",
-  timeout: 50000,
-  withCredentials: true,
+	baseURL: import.meta.env.VITE_BASE_URL,
+	timeout: 50000,
+	withCredentials: true,
 });
