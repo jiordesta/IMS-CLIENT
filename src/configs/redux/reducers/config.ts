533a4c1bb9222ae3ae8fabd6aa33ac5441bd6d4c
Irson <jiordesta@gmail.com>
2026-04-27 12:13:40 +0800

    dark mode default

--- src/configs/redux/reducers/config.ts
+++ src/configs/redux/reducers/config.ts
@@ -2,44 +2,41 @@ import { createSlice } from "@reduxjs/toolkit";
 import { getFromStorage } from "../../../libs/storage";
 
 export const Theme = {
-	LIGHT: "light",
-	DARK: "dark",
+  LIGHT: "light",
+  DARK: "dark",
 };
 
 export const Device = {
-	MOBILE: "mobile",
-	DESKTOP: "desktop",
+  MOBILE: "mobile",
+  DESKTOP: "desktop",
 };
 
 type ConfigState = {
-	theme: (typeof Theme)[keyof typeof Theme];
-	device: (typeof Device)[keyof typeof Device];
-	isNavActive: boolean;
+  theme: (typeof Theme)[keyof typeof Theme];
+  device: (typeof Device)[keyof typeof Device];
+  isNavActive: boolean;
 };
 
 const initialState: ConfigState = getFromStorage("config") || {
-	theme: Theme.LIGHT,
-	device: Device.DESKTOP,
-	isNavActive: true,
+  theme: Theme.DARK,
+  device: Device.DESKTOP,
+  isNavActive: true,
 };
 
 const configSlice = createSlice({
-	name: "config",
-	initialState,
-	reducers: {
-		setTheme: (state) => {
-			state.theme =
-				state.theme === Theme.DARK ? Theme.LIGHT : Theme.DARK;
-		},
-		setDevice: (state, action) => {
-			state.device = action.payload
-				? Device.MOBILE
-				: Device.DESKTOP;
-		},
-		toggleNavigation: (state) => {
-			state.isNavActive = !state.isNavActive;
-		},
-	},
+  name: "config",
+  initialState,
+  reducers: {
+    setTheme: (state) => {
+      state.theme = state.theme === Theme.DARK ? Theme.LIGHT : Theme.DARK;
+    },
+    setDevice: (state, action) => {
+      state.device = action.payload ? Device.MOBILE : Device.DESKTOP;
+    },
+    toggleNavigation: (state) => {
+      state.isNavActive = !state.isNavActive;
+    },
+  },
 });
 
 export const { setTheme, setDevice, toggleNavigation } = configSlice.actions;
