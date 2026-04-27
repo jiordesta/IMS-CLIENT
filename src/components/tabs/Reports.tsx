5ab5935e3ba753483fef0aab8066300423d4c035
Irson <jiordesta@gmail.com>
2026-04-27 11:32:16 +0800

    fix deployment errors

--- src/components/tabs/Reports.tsx
+++ src/components/tabs/Reports.tsx
@@ -1,60 +1,56 @@
-import { useEffect, useState, type Dispatch, type SetStateAction } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import type { AppDispatch, RootState } from "../../configs/redux/store";
-import GlobalTable from "../GlobalTable";
-import { fetchAllReports } from "../../configs/redux/reducers/report";
+import { type Dispatch, type SetStateAction } from "react";
 
 type ReportsTabProps = {
-	setShowLoading: Dispatch<SetStateAction<boolean>>;
+  setShowLoading: Dispatch<SetStateAction<boolean>>;
 };
 
-export default function Reports({ setShowLoading }: ReportsTabProps) {
-	const [refresh, setRefresh] = useState(false);
+export default function Reports({}: ReportsTabProps) {
+  // const [refresh, setRefresh] = useState(false);
 
-	const headers = [
-		{
-			label: "Item",
-			dkey: "productDetails.commonName",
-			col: 4,
-		},
-		{
-			label: "Stocks",
-			dkey: "stocks",
-			col: 4,
-			endLabel: " Boxes",
-		},
-	];
+  // const headers = [
+  // 	{
+  // 		label: "Item",
+  // 		dkey: "productDetails.commonName",
+  // 		col: 4,
+  // 	},
+  // 	{
+  // 		label: "Stocks",
+  // 		dkey: "stocks",
+  // 		col: 4,
+  // 		endLabel: " Boxes",
+  // 	},
+  // ];
 
-	const [filters, setFilters] = useState({
-		date: new Date(new Date().setHours(0, 0, 0, 0)),
-	});
+  // const [filters, setFilters] = useState({
+  // 	date: new Date(new Date().setHours(0, 0, 0, 0)),
+  // });
 
-	const dispatch = useDispatch<AppDispatch>();
+  // const dispatch = useDispatch<AppDispatch>();
 
-	const { accessToken } = useSelector((state: RootState) => state.auth);
+  // const { accessToken } = useSelector((state: RootState) => state.auth);
 
-	const { reports } = useSelector((state: RootState) => state.report);
+  // const { reports } = useSelector((state: RootState) => state.report);
 
-	useEffect(() => {
-		if (!accessToken) return;
-		setShowLoading(true);
-		dispatch(
-			fetchAllReports({ payload: filters, token: accessToken }),
-		).then(() => setShowLoading(false));
-	}, [accessToken, refresh, filters]);
+  // useEffect(() => {
+  // 	if (!accessToken) return;
+  // 	setShowLoading(true);
+  // 	dispatch(
+  // 		fetchAllReports({ payload: filters, token: accessToken }),
+  // 	).then(() => setShowLoading(false));
+  // }, [accessToken, refresh, filters]);
 
-	const handleTableRefresh = () => {
-		setRefresh(!refresh);
-	};
+  // const handleTableRefresh = () => {
+  // 	setRefresh(!refresh);
+  // };
 
-	return (
-		<>Hello</>
-		// <GlobalTable
-		// 	data={reports}
-		// 	headers={headers}
-		// 	filters={filters}
-		// 	setFilters={setFilters}
-		// 	refresh={handleTableRefresh}
-		// />
-	);
+  return (
+    <>Hello</>
+    // <GlobalTable
+    // 	data={reports}
+    // 	headers={headers}
+    // 	filters={filters}
+    // 	setFilters={setFilters}
+    // 	refresh={handleTableRefresh}
+    // />
+  );
 }
